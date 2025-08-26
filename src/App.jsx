7947839018c6a62ba178aa578910e781999efa48
Тарас Антощенко <master.account@botsband.online>
2025-08-26 10:25:27 +0300

    Expand web app -

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React, { useEffect } from 'react'
 import GameMain from './components/GameMain'
 
 function App() {
-  useEffect(() => {
-    if (window.Telegram && window.Telegram.WebApp) {
-      window.Telegram.WebApp.expand()
-    }
-  }, [])
+  
 
   return (
     <GameMain />
