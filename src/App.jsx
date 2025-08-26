0a5893f6250aad1d37f9e481563a77837cb14c8f
Тарас Антощенко <master.account@botsband.online>
2025-08-26 10:29:10 +0300

    Disable vert scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import React, { useEffect } from 'react'
 import GameMain from './components/GameMain'
 
 function App() {
-  
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      const tg = window.Telegram.WebApp
+      tg.expand()
+      tg.disableVerticalScroll(true)
+    }
+  }, [])
 
   return (
     <GameMain />
